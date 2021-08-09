54d8538ac30cc4059c3c64a5c5ed2f04d0e04615
shelbon <mouclepatrick@protonmail.com>
2021-08-09 15:19:02 -0400

    🐞fix: wrong alignment in some  small device

--- src/components/UserInfo.svelte
+++ src/components/UserInfo.svelte
@@ -18,13 +18,16 @@
 
   <ul class="details">
     <li>
-      <p>Followers:{followers}</p>
+      <span class="title">Followers:</span>
+      <span class="title-value">{followers}</span>
     </li>
     <li>
-      <p>Following:{following}</p>
+      <span class="title">Following:</span>
+      <span class="title-value">{following}</span>
     </li>
     <li>
-      <p>Playlists:{nbPlaylist}</p>
+      <span class="title">Playlists:</span>
+      <span class="title-value">{nbPlaylist}</span>
     </li>
   </ul>
 </div>
@@ -32,36 +35,68 @@
 <style>
   .picture {
     border-radius: 50%;
-    min-height: 10rem;
-    min-width: 16.4rem;
-    max-width: 30%;
+    max-height: 150px;
+    min-width: 150px;
   }
   .user-info {
-    align-self: center;
     display: flex;
     flex-flow: column;
     justify-content: flex-start;
     align-items: center;
     margin-bottom: 2.9rem;
-    width: 50%;
+  }
+  span {
+    font-size: 1.92rem;
+  }
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    margin-bottom: 1.6rem;
+  }
+  li > * {
+    align-self: center;
+  }
+  .title-value {
+    margin-left: 0;
+    font-weight: 600;
   }
   .details {
     display: flex;
-    flex-flow: row wrap;
+    flex-flow: column;
+    justify-content: space-between;
+    padding: 0;
   }
-  .details > * {
-    margin: 0 3.3rem 0 0;
-  }
+
   .name {
     text-align: center;
+    text-transform: capitalize;
   }
+
   .user-info :global(svg) {
     border: 2px solid currentcolor;
     padding: 1rem;
     border-radius: 50%;
     min-height: 10rem;
     min-width: 16.4rem;
-    width: 30%;
     height: 100%;
+  }
+  @media (min-width: 768px) {
+    .details {
+      display: flex;
+      flex-flow: row;
+    }
+    li {
+      flex-direction: row;
+      margin-bottom: 0;
+      margin-right: 3.3rem;
+    }
+    li > * {
+      align-self: auto;
+    }
+    .title-value {
+      margin-left: 1rem;
+    }
   }
 </style>
